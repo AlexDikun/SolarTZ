07bf5b3cf222765f0f372e45da7b33828b34c3d9
AlexDikun <loban.sasha.97@mail.ru>
2025-09-03 22:04:50 +0300

    адаптировал JS скрипт для автоматического заполнения странички карточками: теперь дублиурем только картчоки .ad-card вместо всего HTML внутри adsGrid - цикл производит четыре клонирования, на данный момент этого хватит чтобы заполнить всю страничку

--- mainpage/scripts/main.js
+++ mainpage/scripts/main.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const adsGrid = document.getElementById('adsGrid');
-    const ads = adsGrid.innerHTML; 
-    adsGrid.innerHTML = ads.repeat(4);
+    const adCards = adsGrid.querySelectorAll('.ad-card');
+
+    adCards.forEach(card => {
+        for (let i = 0; i < 3; i++ ) {
+            const clone = card.cloneNode(true);
+            adsGrid.appendChild(clone)
+        }
+    });
 });
