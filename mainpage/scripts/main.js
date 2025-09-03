a20a91f61fb9c134c2d36a239bb1078d640fed45
AlexDikun <loban.sasha.97@mail.ru>
2025-09-03 22:24:10 +0300

    исправил ошибку в скрипте JS - теперь цикл фор следит за количеством пар карточек, а цикл форИч копирует каждую карточку, чередуя их

--- mainpage/scripts/main.js
+++ mainpage/scripts/main.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const adsGrid = document.getElementById('adsGrid');
     const adCards = adsGrid.querySelectorAll('.ad-card');
 
-    adCards.forEach(card => {
-        for (let i = 0; i < 3; i++ ) {
+    for (let i = 0; i < 3; i++ ) {
+        adCards.forEach(card => {
             const clone = card.cloneNode(true);
             adsGrid.appendChild(clone)
-        }
-    });
+        });
+    }
 });
