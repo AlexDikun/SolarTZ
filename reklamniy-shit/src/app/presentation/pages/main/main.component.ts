6647d7dd079cc85e18efb59c53fd5d5353a9a334
AlexDikun <loban.sasha.97@mail.ru>
2025-09-22 00:39:45 +0300

    побаловался с пайпами

--- reklamniy-shit/src/app/presentation/pages/main/main.component.ts
+++ reklamniy-shit/src/app/presentation/pages/main/main.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { MainBsService } from './services/main-bs.service';
+import { DatePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [
+    DatePipe,
+    CurrencyPipe
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   providers: [
@@ -11,5 +15,7 @@ import { MainBsService } from './services/main-bs.service';
   ]
 })
 export class MainComponent  {
-
+  curDate :Date = new Date();
+  price1 :number = 5000;
+  price2 = 'бесплатно';
 }
