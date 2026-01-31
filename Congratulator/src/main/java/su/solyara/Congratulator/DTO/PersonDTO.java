f345c1ca7402abac9158421362a05113a1b1d7a7
AlexDikun <loban.sasha.97@mail.ru>
2026-01-31 19:25:25 +0300

    добавил метод FromEntity

--- Congratulator/src/main/java/su/solyara/Congratulator/DTO/PersonDTO.java
+++ Congratulator/src/main/java/su/solyara/Congratulator/DTO/PersonDTO.java
@@ -1,5 +1,6 @@
 package su.solyara.Congratulator.DTO;
 
+import su.solyara.Congratulator.domain.PersonEntity;
 import su.solyara.Congratulator.domain.positions.Position;
 
 import lombok.Data;
@@ -10,4 +11,13 @@ public class PersonDTO {
     private String lastName;
     private String email;
     private Position position;
+
+    public static PersonDTO fromEntity(PersonEntity personEntity) {
+        PersonDTO dto = new PersonDTO();
+        dto.setEmail(personEntity.getEmail());
+        dto.setFirstName(personEntity.getFirstName());
+        dto.setLastName(personEntity.getLastName());
+        dto.setPosition(personEntity.getPosition());
+        return dto;
+    }
 }
