5a76cbc8a1562fad2729898f7c3dff309b6627a1
AlexDikun <loban.sasha.97@mail.ru>
2026-01-31 19:13:23 +0300

    добавил метод поиска по эмейлу

--- Congratulator/src/main/java/su/solyara/Congratulator/repos/PersonRepo.java
+++ Congratulator/src/main/java/su/solyara/Congratulator/repos/PersonRepo.java
@@ -1,7 +1,11 @@
 package su.solyara.Congratulator.repos;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import su.solyara.Congratulator.domain.PersonEntity;
 
-public interface PersonRepo extends JpaRepository <PersonEntity, Long> {}
+public interface PersonRepo extends JpaRepository <PersonEntity, Long> {
+    Optional<PersonEntity> findByEmail(String email);
+}
