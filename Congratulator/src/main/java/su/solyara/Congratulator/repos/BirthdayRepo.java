23ecc96c156685f8a587d0f0ae97906d4a462df5
AlexDikun <loban.sasha.97@mail.ru>
2026-01-31 22:11:38 +0300

    День рождения - это не сущность, а свойство человека. Потому и вынес это в поле класса PersonEntity. Моя архитектурная ошибка

--- Congratulator/src/main/java/su/solyara/Congratulator/repos/BirthdayRepo.java
+++
@@ -1,7 +0,0 @@
-package su.solyara.Congratulator.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import su.solyara.Congratulator.domain.BirthdayEntity;
-
-public interface  BirthdayRepo extends JpaRepository <BirthdayEntity, Long> {}
