fd02ba1fb1fd40084675e8e44b43ec28e120fa7e
AlexDikun <loban.sasha.97@mail.ru>
2026-02-01 00:43:17 +0300

    добавил класс, котоырй при сборке приложения загружает данные - в конкретном случае, демо-список сотрудников

---
+++ Congratulator/src/main/java/su/solyara/Congratulator/config/SetupDataLoader.java
@@ -0,0 +1,83 @@
+package su.solyara.Congratulator.config;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.lang.NonNull;
+
+import su.solyara.Congratulator.DTO.PersonDTO;
+import su.solyara.Congratulator.domain.positions.Position;
+import su.solyara.Congratulator.service.PersonService;
+
+@Component
+public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
+    private boolean alreadySetup = false;
+
+    @Autowired
+    private PersonService personService;
+
+    @Override
+    @Transactional
+    public void onApplicationEvent(final @NonNull ContextRefreshedEvent event) {
+        if (alreadySetup) {
+            return;
+        }
+
+        List<PersonDTO> demoPersons = List.of(
+            new PersonDTO(
+                "Анна",
+                "Иванова",
+                "anna.ivanova@example.com",
+                LocalDate.of(1990, 5, 15),
+                Position.MANAGER
+            ),
+            new PersonDTO(
+                "Иван",
+                "Петров",
+                "ivan.petrov@example.com",
+                LocalDate.of(1985, 8, 22),
+                Position.ENGINEER
+            ),
+            new PersonDTO(
+                "Мария",
+                "Сидорова",
+                "maria.sidorova@example.com",
+                LocalDate.of(1992, 12, 3),
+                Position.ADMINISTRATOR
+            ),
+            new PersonDTO(
+                "Джон",
+                "Сноу",
+                "jhon.show@example.com",
+                LocalDate.of(567, 12, 3),
+                Position.SUPPORT_STAFF
+            ),
+            new PersonDTO(
+                "Саша",
+                "Грей",
+                "sasha.grey@example.com",
+                LocalDate.of(1996, 5, 8),
+                Position.ADMINISTRATOR
+            ),
+            new PersonDTO(
+                "Джозеф Огастус",
+                "Зарелли",
+                "zarelli@example.com",
+                LocalDate.of(1947, 01, 01),
+                Position.HSSE_SPECIALIST
+            )
+        );
+
+        for (PersonDTO dto : demoPersons) {
+            personService.createPerson(dto);
+        }
+       
+        alreadySetup = true;
+    }
+
+}
