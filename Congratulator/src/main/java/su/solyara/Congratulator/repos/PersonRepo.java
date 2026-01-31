04bf206a4607d44ad0f2176a4660e2edaecf6f88
AlexDikun <loban.sasha.97@mail.ru>
2026-02-01 00:09:35 +0300

    добавил метод для проверки использование емейла  каким-либо пользователем

--- Congratulator/src/main/java/su/solyara/Congratulator/repos/PersonRepo.java
+++ Congratulator/src/main/java/su/solyara/Congratulator/repos/PersonRepo.java
@@ -7,5 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import su.solyara.Congratulator.domain.PersonEntity;
 
 public interface PersonRepo extends JpaRepository <PersonEntity, Long> {
+
     Optional<PersonEntity> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
 }
