a3a9b0039f9583e71c07c8a99ef897d3a690fcb3
AlexDikun <loban.sasha.97@mail.ru>
2026-01-31 18:16:23 +0300

    добавил две сущности и перечисление для должностей

---
+++ Congratulator/src/main/java/su/solyara/Congratulator/domain/PersonEntity.java
@@ -0,0 +1,32 @@
+package su.solyara.Congratulator.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import su.solyara.Congratulator.domain.positions.Position;
+
+@Entity
+@Table(name="persons")
+@Data
+public class PersonEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "positions")
+    private Position position;
+     
+}
---
+++ Congratulator/src/main/java/su/solyara/Congratulator/domain/positions/Position.java
@@ -0,0 +1,14 @@
+package su.solyara.Congratulator.domain.positions;
+
+public enum Position {
+    EXECUTIVE,          // Руководитель
+    ADMINISTRATOR,      // Администратор
+    PRODUCTION_WORKER,  // Производственный рабочий
+    ENGINEER,           // Инженерно‑технический работник (ИТР)
+    QUALITY_SPECIALIST, // Специалист по контролю качества
+    LOGISTICIAN,        // Логист
+    HSSE_SPECIALIST,    // Специалист по охране труда и экологии (HSSE)
+    SUPPORT_STAFF,      // Вспомогательный персонал
+    RESEARCHER,         // Научный сотрудник (НИОКР)
+    MANAGER             // Специалист по маркетингу и продажам
+}
