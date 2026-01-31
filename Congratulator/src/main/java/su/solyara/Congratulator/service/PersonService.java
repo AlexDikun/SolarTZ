c5270a967e62cedd4327733e4b534b60a00a8fda
AlexDikun <loban.sasha.97@mail.ru>
2026-02-01 00:10:23 +0300

    добавил методы по обновлению записи персоны и ее удалению

--- Congratulator/src/main/java/su/solyara/Congratulator/service/PersonService.java
+++ Congratulator/src/main/java/su/solyara/Congratulator/service/PersonService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.EntityNotFoundException;
 import su.solyara.Congratulator.DTO.PersonDTO;
 import su.solyara.Congratulator.domain.PersonEntity;
 import su.solyara.Congratulator.domain.exceptions.ConflictException;
@@ -37,4 +38,40 @@ public class PersonService {
 
         return PersonDTO.fromEntity(personEntity);
     } 
+
+    public PersonDTO updatePerson(Long id, PersonDTO personDTO) {
+        Optional<PersonEntity> optPerson = personRepo.findByEmail(personDTO.getEmail());
+
+        if (optPerson.isPresent()) {
+            throw new EntityNotFoundException("Сотрудник с id=" + id + " не найден");
+        }
+        
+        if (personRepo.existsByEmail(personDTO.getEmail())) {
+            throw new ConflictException(
+            "Email '" + personDTO.getEmail() + "' уже используется другим сотрудником"
+            );
+        }
+
+        PersonEntity personEntity = optPerson.get();
+
+        personEntity.setEmail(personDTO.getEmail());
+        personEntity.setFirstName(personDTO.getFirstName());
+        personEntity.setLastName(personDTO.getLastName());
+        personEntity.setBirthDate(personDTO.getBirthDate());
+        personEntity.setPosition(personDTO.getPosition());
+
+        PersonEntity updated = personRepo.save(personEntity);
+        return PersonDTO.fromEntity(updated);
+    }
+
+    public void deletePerson(Long id) {
+        if (!personRepo.existsById(id)) {
+            throw new EntityNotFoundException("Сотрудник с id=" + id + " не найден");
+        }
+        personRepo.deleteById(id);
+    }
+
+
+
+
 }
